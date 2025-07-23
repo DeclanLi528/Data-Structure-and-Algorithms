2c081f8d612810970da971536e6cdebbc2677a1c
Declan Li <DeclanLi528@gmail.com>
2025-07-23 15:06:24 +0800

    Add notes to Printer/Task/Simulation

--- queue.py
+++ queue.py
@@ -28,12 +28,12 @@ print(hot_potato(["Bill", "David", "Susan", "Jane", "Kent", "Brad"], 7))
 ### printing mission
 class Printer:
     def __init__(self, ppm):
-        self.page_rate = ppm
-        self.current_task = None
-        self.time_remaining = 0
+        self.page_rate = ppm #set pages per minute
+        self.current_task = None # initialize the task situation
+        self.time_remaining = 0 # initialize
 
     def tick(self):
-        if self.current_task is not None:
+        if self.current_task is not None: 
             self.time_remaining = self.time_remaining - 1
             if self.time_remaining <= 0:
                 self.current_task = None
@@ -67,14 +67,14 @@ def simulation(num_seconds, pages_per_minute):
     waiting_time = []
 
     for current_second in range(num_seconds):
-        if new_print_task():
-            task = Task(current_second)
+        if new_print_task(): # For each second try if 1/180 is possible
+            task = Task(current_second)  # Create a timestamp here
             print_queue.enqueue(task)
 
         if (not lab_printer.busy()) and (not print_queue.is_empty()):
             nexttask = print_queue.dequeue()
-            waiting_time.append(nexttask.wait_time(current_second))
-            lab_printer.start_next(nexttask)
+            waiting_time.append(nexttask.wait_time(current_second)) # give current time to wait_time
+            lab_printer.start_next(nexttask) # Give printer a new task so that tick starts
 
         lab_printer.tick()
 
