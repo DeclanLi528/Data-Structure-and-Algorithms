da853f89cf31789571deb1e3b3712bc24a880676
Declan Li <DeclanLi528@gmail.com>
2025-07-28 21:39:10 +0800

    将十进制数转换成任意进制数

--- 数字转二进制.py
+++ 数字转二进制.py
@@ -18,3 +18,23 @@ binary_result, remainders = decimal_to_binary(number)
 print(f"十进制 {number} 的二进制表示是:{binary_result}")
 print(f"转换过程中的余数是:{remainders}")
 
+
+
+# 将十进制数转换成任意进制数
+from pythonds3.basic import Stack
+
+def base_converter(decimal_num, base):
+    digits = '0123456789ABCDEF'
+    rem_stack = Stack()
+
+    while decimal_num > 0:
+        rem = decimal_num % base
+        rem_stack.push(rem)
+        decimal_num = decimal_num // base
+
+    
+    new_string = ''
+    while not rem_stack.is_empty():
+        new_string = new_string + digits[rem_stack.pop()]
+
+    return new_string
